b5a0c7148837ddc79ab2b4420c86decea16b1936
ShenghuiZhou <ShenghuiZhou@users.noreply.github.com>
2015-08-16 12:11:32 -0500

    Update 1.cpp

--- ch1/1.cpp
+++ ch1/1.cpp
@@ -1,20 +1,3 @@
-/*----------------------------------------------------
-> File Name:    1.cpp
-> Author:       Shenghui Zhou
-> Mail:         shzhouus@gmail.com 
-> Created Time: Wed 12 Aug 2015 09:24:02 AM CDT
-----------------------------------------------------*/
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <unordered_map>
-#include <utility>
-#include <iterator>
-
-using namespace std;
-
 bool isUniqueChars(const string &str) {
 	int check = 0;
 	for(auto c:str) {
