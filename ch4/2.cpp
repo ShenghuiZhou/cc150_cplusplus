7853dbf8aa37f5f2eac3b53296a59f0d4be5e93b
Shenghui Zhou <shzhouus@gmail.com>
2015-08-16 23:12:19 -0500

    ch4

--- ch4/2.cpp
+++ ch4/2.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
+#include <unordered_map>
 #include <unordered_set>
 
 using namespace std;
 
-int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
-	unordered_set<string> set1 = {beginWord}, set2 = {endWord};
-	unordered_set<string> *p1 = &set1, *p2 = &set2;
-	int ret = 1;
+bool solution_2(int start, int end, unordered_map<int,unordered_set<int>> &G) {
+	unordered_map<int,bool> check;
+	for(auto iter = G.begin(); iter!=G.end(); ++iter) check[iter->first] = false;
+	check[start] = check[end] = true;
+	unordered_set<int> set1 = {start}, set2 = {end};
+	unordered_set<int> *p1 = &set1, *p2 = &set2;
+	
 	while(!set1.empty() && !set2.empty()) {
-		++ret;
-		if(p2->size()<p1->size()) swap(p1,p2);
-		unordered_set<string> tmp;
-		for(auto str:*p1) {
-			for(int i=0; i!=str.size(); ++i) {
-				for(int j=0; j!=26; ++j) {
-					char c= 'a' + j;
-					if(c!=str[i]) {
-						swap(str[i],c);
-						if(p2->find(str)!=p2->end()) return ret;
-						auto iter = wordDict.find(str);
-						if(iter!=wordDict.end()) {
-							wordDict.erase(iter);
-							tmp.insert(str);
-						}
-						swap(str[i],c);
-					}
+		if(set1.size()>set2.size()) swap(p1,p2);
+		unordered_set<int> tmp;
+		for(auto i:*p1) {
+			for(auto j:G[i]) {
+				if(p2->find(j)!=p2->end()) return true;
+				if(check[j]==false) {
+					check[j] = true;
+					tmp.insert(j);
 				}
 			}
 		}
 		*p1 = tmp;
 	}
-	return 0;
+
+	return false;
 }
